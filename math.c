ee1143811d911d41039eb1e11025cf8d3e9d24cc
Eduardo <eduardo.acuna@iteso.mx>
2022-10-17 14:26:39 -0500

    new fun doubleDouble

--- math.c
+++ math.c
@@ -3,8 +3,17 @@
 #include "math.h"
 
  int dobleInt (int a) { 
-    if (a MAX_INT) { 
+    if (a > MAX_INT) { 
     return -1; 
     }
  return a*2;
  }
+
+double doubleDouble (double a) {
+    if (a > MAX_DOUBLE) { 
+        return -1;
+    }
+    return a*2; 
+
+}
+
