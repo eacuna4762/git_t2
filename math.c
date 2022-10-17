772b2803269ff5e228c3046cde63b69aacb8107a
Eduardo <eduardo.acuna@iteso.mx>
2022-10-17 13:31:52 -0500

    math.c and math.h

---
+++ math.c
@@ -0,0 +1,8 @@
+ /* Funciones matemáticas para nuestro codigo */ 
+
+ int dobleInt (int a) { 
+    if (a MAX_INT) { 
+    return -1; 
+    }
+ return a*2;
+ }
---
+++ math.h
@@ -0,0 +1,5 @@
+/*Encabezados de Math.c */
+
+#define MAX_INIT (2^sizeof(int))/2 
+
+int dobleInt(int a);
